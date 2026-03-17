bd636f10baf65d2b018a4798d0aec2a798b35abb
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-17 22:45:42 +0530

    Day 7 Excersie 1 - Create a Calculator

---
+++ Day 7 - Excercise 1 Create a Calculator/main.py
@@ -0,0 +1,35 @@
+n = 15
+m = 6
+print("The first value is: ", n)
+print("The second value is: ", m)
+
+print("Addition")
+ans1 = n + m
+print("The addition of",n,"and", m ,"is", ans1)
+
+print("Subtraction")
+ans2 = n - m
+print("The addition of",n,"and", m ,"is", ans2)
+
+print("Multiplication")
+ans3 = n * m
+print("The addition of",n,"and", m ,"is", ans3)
+
+print("Divison")
+ans4 = n / m
+print("The addition of",n,"and", m ,"is", ans4)
+
+print("Modules")
+ans5 = n % m
+print("The addition of",n,"and", m ,"is", ans5)
+
+print("Floor Divison")
+ans6 = n + m
+print("The addition of",n,"and", m ,"is", ans6)
+
+print("Exponential")
+ans7 = n ** m
+print("The addition of",n,"and", m ,"is", ans7)
+
+
+
