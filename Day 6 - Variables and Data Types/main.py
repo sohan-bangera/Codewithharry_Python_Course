57b87146bd0ba93de73322f8d0f2427f29757617
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-17 22:31:48 +0530

    Day 6 - Variables and Data Types added

---
+++ Day 6 - Variables and Data Types/main.py
@@ -0,0 +1,23 @@
+a = complex(1.1, 2)
+print(a)
+b = True
+c = "Harry"
+d = None
+
+list1 = [8,2,[3,2],["hello", "World"]]
+print(list1)
+
+tup = (1,2,("as", "nk"), (10,11))
+print(tup)
+
+dic = {"name":"Sohan", "age":25,"job":"working"}
+print(dic)
+
+print("The type of a is: ", type(a))
+print("The type of b is: ", type(b))
+print("The type of c is: ", type(c))
+print("The type of d is: ", type(d))
+print("The type of list1 is: ", type(list1))
+print("The type of tup is: ", type(tup))
+print("The type of dic is: ", type(dic))
+
