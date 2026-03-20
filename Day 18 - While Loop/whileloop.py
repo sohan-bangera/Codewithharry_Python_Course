15e2e14069a6e01d42d30409d9275e45eab0a96e
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-20 10:20:53 +0530

    Day 18 added to repo

---
+++ Day 18 - While Loop/whileloop.py
@@ -0,0 +1,17 @@
+# i = 0
+# while(i<3):
+#     print(i)
+#     i = i + 1
+
+# x = 5
+# while (x > 0):
+#     print(x)
+#     x = x - 1
+# else:
+#     print('counter is 0')
+
+while True:
+    num = int(input("Enter the positive number: "))
+    print(num)
+    if num < 0:
+        break
