dd867e47fb6e6454af927307e621416ffb288b17
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-20 10:00:57 +0530

    Day 17 is added to repo

---
+++ Day 17 - For Loop/for loop.py
@@ -0,0 +1,22 @@
+# for i in range(10):
+#     print(i, end=", ")
+
+# name = 'Abhishek'
+# for i in name:
+#     print(i, end=", ")
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for x in colors:
+#     print(x)
+#     for y in x:
+#         print(y, end=", ")
+#     print('\n')
+
+for k in range(10,0, -1):
+    print(k)
+
+for j in range(1, 11):
+    print(f"5 x {j} = {5 * j}")
+
+name = "Sohan"
+print(f"My name is {name} and this name {name} is very useful")
