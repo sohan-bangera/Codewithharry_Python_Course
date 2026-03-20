fd6ed248af0380a68929017ff9e4a708ea3fc026
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-20 21:55:43 +0530

    Day 20 added to repo

---
+++ Day 20 - Functions/function.py
@@ -0,0 +1,17 @@
+def name(fname, lname):
+    print("Hello", fname, lname)
+
+name("Sohan", "Bangera")
+
+def greaterNumberOrNot(a, b):
+    if(a>b):
+        print(a,"iss greater than",b)
+    else:
+        print(b,"is greater than",a)
+
+greaterNumberOrNot(10,13)
+greaterNumberOrNot(143,3)
+
+def isLesser(a,b):
+    pass
+print("hello")
