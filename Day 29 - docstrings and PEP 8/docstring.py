72c1d7f2fac16ad3d821f135c5f40713ace8ad2c
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-22 21:39:31 +0530

    Day 29 is added to repo

---
+++ Day 29 - docstrings and PEP 8/docstring.py
@@ -0,0 +1,7 @@
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+square(5)
+
+print(square.__doc__)
+#docstring are defined right below the function name, if this is added before the function name or in the middle of the function then it will be treated as comments and not doc string
