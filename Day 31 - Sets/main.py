ef0de6a713940b7758a75965104345756e8a744f
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-24 10:52:36 +0530

    Day 31 added to repo

---
+++ Day 31 - Sets/main.py
@@ -0,0 +1,16 @@
+s = {2,3,4,4,5,2,4,2,5,6,7,8,1}
+print(s)
+
+# for i in s:
+#     print(i)
+
+p = {} #this gives type as dict
+print(type(p))
+
+#to make a empty set
+h = set([1,2,3,3,3,5])
+print(type(h))
+print(h)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
