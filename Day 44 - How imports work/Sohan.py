eaa69a8c734ca3d1fc891c308da4ca08a982a07f
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-29 13:04:41 +0530

    Day 44 added to repo

---
+++ Day 44 - How imports work/Sohan.py
@@ -0,0 +1,4 @@
+def Welcome():
+    print("Welcome to the world of python")
+
+Sohan = "My name is Sohan Bangera"
---
+++ Day 44 - How imports work/main.py
@@ -0,0 +1,11 @@
+import math
+from Sohan import Welcome, Sohan
+# from math import sqrt as s, pi
+
+# c = s(9)
+# print(c)
+# print(pi)
+
+Welcome()
+print(Sohan)
+print(dir(math))
