a0db20092b3aca510b978481a8707c63021cb87c
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-30 11:04:28 +0530

    Day 49 added to repo

---
+++ Day 49 - File IO/main.py
@@ -0,0 +1,15 @@
+# # read mode
+# f = open('myfile.txt', 'rb')
+# # print(f)
+
+# text = f.read()
+# print(text)
+# f.close()
+
+# Writing to a file
+f = open('myfile2.txt', 'a')
+f.write("This is file22 and io am writing content into it and i am sohan")
+# text = f.read()
+# print(text)
+f.close()
+
