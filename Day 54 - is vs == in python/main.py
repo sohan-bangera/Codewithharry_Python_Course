b21b5fd82ffcf5197312d2935fdd9c2ac517c1df
Sohan Bangera <sohanbangera2001@gmail.com>
2026-03-31 11:08:35 +0530

    Day 54 added to repo

---
+++ Day 54 - is vs == in python/main.py
@@ -0,0 +1,17 @@
+a = 4
+b = "4"
+
+print(a is b) #False
+print(a == b) #False
+
+c = [1,2,3,4]
+d = [1,2,3,4]
+
+print(c is d) #False
+print(c == d) #True
+
+e = 3
+f = 3
+
+print(e is f) #True
+print(e == f) #True
