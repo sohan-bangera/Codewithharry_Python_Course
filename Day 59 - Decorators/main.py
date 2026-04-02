9b33b1fdd3805cc9813f6e2e0c20fa06e46190ed
Sohan Bangera <sohanbangera2001@gmail.com>
2026-04-02 11:24:24 +0530

    Day 59 added to repo

--- Day 59 - Decorators/main.py
+++ Day 59 - Decorators/main.py
@@ -18,3 +18,20 @@ def add(a,b):
 
 hello()
 add(1,2)
+
+import logging
+
+def log_function_call(func):
+    def decorated(*args, **kwargs):
+        logging.info(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
+        result = func(*args, **kwargs)
+        logging.info(f"{func.__name__} returned {result}")
+        return result
+    return decorated
+
+@log_function_call
+def my_function(a, b):
+    return a + b
+
+a = my_function(1,4)
+print(a)
