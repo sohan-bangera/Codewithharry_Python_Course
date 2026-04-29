13cb686ace8872da506ca68c13b768dfd1f3f943
Sohan Bangera <sohanbangera2001@gmail.com>
2026-04-29 22:26:48 +0530

    Day 87 added to repo

---
+++ Day 87 - Shutil Module/main.py
@@ -0,0 +1,17 @@
+import shutil
+
+shutil.copy("main.py", "main2.py")
+
+import shutil
+
+# Copying a file
+shutil.copy("src.txt", "dst.txt")
+
+# Copying a directory
+shutil.copytree("src_dir", "dst_dir")
+
+# Moving a file
+shutil.move("src.txt", "dst.txt")
+
+# Deleting a directory
+shutil.rmtree("dir")
