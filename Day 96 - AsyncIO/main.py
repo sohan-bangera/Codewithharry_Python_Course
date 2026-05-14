dce8ba70d30b33fe073d86c1b494882877f22b55
Sohan Bangera <sohanbangera2001@gmail.com>
2026-05-14 22:17:18 +0530

    Day 96 added to repo

---
+++ Day 96 - AsyncIO/main.py
@@ -0,0 +1,12 @@
+import asyncio
+
+async def my_async_function():
+    # asynchronous code here
+    await asyncio.sleep(1)
+    return "Hello, Async World!"
+
+async def main():
+    result = await my_async_function()
+    print(result)
+
+asyncio.run(main())
